c634ecfcae97c1cd3dae739440ebf2c9e3da6289
Andy Irvine <andy@zooz.com>
2017-05-08 18:16:22 +0100

    add README and disable report output

---
+++ README.md
@@ -0,0 +1,26 @@
+## Synopsis
+This tools checks the ACLs of AWS buckets to see if any permissions have been assigned that do not match policy.
+
+## Usage
+```
+chmod +x audit.s3.sh
+./audits3.sh
+
+Number of Buckets: 9
+  1/9   : andy-perm-test                                                   : FAIL 
+  2/9   : andy-perm-test-2                                                 : FAIL 
+  3/9   : andywirv-cloudtrail                                              : PASS 
+  4/9   : bt-mp3                                                           : PASS 
+  5/9   : s3-build-cache-example                                           : PASS 
+  6/9   : tfb8sdjaumo8y4n3akmg                                             : PASS 
+  7/9   : vennart-com-test                                                 : PASS 
+  8/9   : wordpress-account                                                : PASS 
+
+```
+
+## Installation
+
+You need:
+ AWS cli tools      http://docs.aws.amazon.com/cli/latest/userguide/installing.html
+ jq cli https://stedolan.github.io/jq/
+
--- audits3.sh
+++ audits3.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# This script uses the AWS cli tools (and the credentials you have configured in ~/.aws/credentials)
+# specify the profile to use as first argument. Optionally leave blank to use default profile
 PROFILE=${1:-default}
 
 #Output constants
@@ -7,29 +9,41 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
+
 # Create Output file
-filename=$(date +%Y%m%d_%H%M)-report.json
-echo '{"permission_issues": []}' > ./$filename
+#filename=$(date +%Y%m%d_%H%M)-report.json
+#echo '{"permission_issues": []}' > ./$filename
 
 # Get list of S3 buckets from AWS
 buckets=$(aws --profile $PROFILE s3api list-buckets | jq -r '.Buckets[] .Name');
 bucketArray=($buckets);
-echo 'Number of Buckets:' ${#bucketArray[@]}
+numBuckets=${#bucketArray[@]}
+printf 'Number of Buckets: %s\n' "$numBuckets"
+
+
 
 #Loop through buckets checking for permissions that allow allow all authenticated users to access (any permisisons)
 for ((i=0; i<${#bucketArray[@]}; ++i));
-    do  
-       acl=$(aws --profile $PROFILE s3api  get-bucket-acl --bucket ${bucketArray[$i]});
-       bucketname=${bucketArray[$i]}
-       bucketinstance=$(jq --arg BCKT_NAME ${bucketname} '. | select( .Grants[] .Grantee .URI == "http://acs.amazonaws.com/groups/global/AuthenticatedUsers") | .Bucket=$BCKT_NAME' <<< "$acl")
-    if [ -n "$bucketinstance" ]
-        then
-            result="FAIL"
-            colour=$RED
-            cat $filename | jq  --argjson BCK_INST "${bucketinstance}" '.permission_issues |= . + [$BCK_INST]'  | tee  $filename >>/dev/null
+    do
+        bucketregion=$(jq --raw-output '.LocationConstraint' <<< $(aws s3api  --profile $PROFILE get-bucket-location --bucket ${bucketArray[$i]}))
+        if [ "$bucketregion" != "null" ]
+            then
+                acl=$(aws --region $bucketregion --profile $PROFILE s3api  get-bucket-acl --bucket ${bucketArray[$i]});
+            else
+                acl=$(aws --profile $PROFILE s3api  get-bucket-acl --bucket ${bucketArray[$i]});
+        fi
+        bucketname=${bucketArray[$i]}
+        bucketinstance=$(jq --arg BCKT_NAME ${bucketname} '. | select( .Grants[] .Grantee .URI == "http://acs.amazonaws.com/groups/global/AuthenticatedUsers") | .Bucket=$BCKT_NAME' <<< "$acl")
+
+        if [ -n "$bucketinstance" ]
+            then
+                result="FAIL"
+                colour=$RED
+                #cat $filename | jq  --argjson BCK_INST "${bucketinstance}" '.permission_issues |= . + [$BCK_INST]'  | tee  $filename >>/dev/null
         else
             result='PASS'
-            colour=$GREEN            
-    fi
-    printf "%-64s :$colour %s $NC\n" "$bucketname" "$result";
+            colour=$GREEN
+        fi
+        
+        printf "%3s/%-3s : %-64s :$colour %s $NC\n" "$(($i + 1))" "$numBuckets" "$bucketname" "$result";
 done;
