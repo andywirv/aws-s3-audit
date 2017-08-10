e107902d36ee6ef7effb8d42d8c219d1d5bc4d57
Andy Irvine <andy@zooz.com>
2017-08-10 18:08:49 +0100

    check for everyone permissions as well as all authenticated users

--- audits3.sh
+++ audits3.sh
@@ -2,17 +2,24 @@
 
 # This script uses the AWS cli tools (and the credentials you have configured in ~/.aws/credentials)
 # specify the profile to use as first argument. Optionally leave blank to use default profile
-PROFILE=${1:-default}
 
-#Output constants
+
+#Output constants used in formatting
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
+trap "exit" INT TERM
+trap "printf 'Exiting...\n' 1>&2 && kill 0" EXIT
+set -e
 
-# Create Output file
-#filename=$(date +%Y%m%d_%H%M)-report.json
-#echo '{"permission_issues": []}' > ./$filename
+PROFILE=${1:-default}
+if [ -z "$1" ]
+    then
+        printf "\nno profile specified so using 'default'\n"
+fi
+printf "AWS Profile: $PROFILE\n"
+
 
 # Get list of S3 buckets from AWS
 buckets=$(aws --profile $PROFILE s3api list-buckets | jq -r '.Buckets[] .Name');
@@ -20,9 +27,9 @@ bucketArray=($buckets);
 numBuckets=${#bucketArray[@]}
 printf 'Number of Buckets: %s\n' "$numBuckets"
 
-
-
-#Loop through buckets checking for permissions that allow allow all authenticated users to access (any permisisons)
+#  Loop through buckets checking for permissions that allow eiher:
+#  1. Any AWS Authenticated user http://acs.amazonaws.com/groups/global/AuthenticatedUsers. This is bad
+#  2. Everyone  "http://acs.amazonaws.com/groups/global/AllUsers. This is even worse unless is intended to be public.
 for ((i=0; i<${#bucketArray[@]}; ++i));
     do
         bucketregion=$(jq --raw-output '.LocationConstraint' <<< $(aws s3api  --profile $PROFILE get-bucket-location --bucket ${bucketArray[$i]}))
@@ -33,13 +40,16 @@ for ((i=0; i<${#bucketArray[@]}; ++i));
                 acl=$(aws --profile $PROFILE s3api  get-bucket-acl --bucket ${bucketArray[$i]});
         fi
         bucketname=${bucketArray[$i]}
-        bucketinstance=$(jq --arg BCKT_NAME ${bucketname} '. | select( .Grants[] .Grantee .URI == "http://acs.amazonaws.com/groups/global/AuthenticatedUsers") | .Bucket=$BCKT_NAME' <<< "$acl")
-
+        bucketinstance=$(jq --arg BCKT_NAME ${bucketname} \
+                        '. | select( .Grants[] .Grantee .URI == "http://acs.amazonaws.com/groups/global/AuthenticatedUsers" or .Grants[] .Grantee .URI == "http://acs.amazonaws.com/groups/global/AllUsers") | .Bucket=$BCKT_NAME' \
+                        <<< "$acl")
+        
         if [ -n "$bucketinstance" ]
             then
                 result="FAIL"
                 colour=$RED
-                #cat $filename | jq  --argjson BCK_INST "${bucketinstance}" '.permission_issues |= . + [$BCK_INST]'  | tee  $filename >>/dev/null
+                echo $bucketname >> failed.txt
+                echo $acl >> failed.txt
         else
             result='PASS'
             colour=$GREEN
